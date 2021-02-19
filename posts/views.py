035ea80395fc4f601ab89543fccd459582cf5bab
Amirak <akbariamirreza2001@gmail.com>
2021-02-19 20:58:55 +0330

    top right carousel done

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.views import View
 from .models import Post
@@ -10,6 +12,9 @@ class IndexPage(View):
 
     def get(self, request, *args, **kwargs):
         context = {}
-        four_most_views = self.model.objects.order_by("views")[:4]
+        four_most_views = self.model.objects.filter(date_published__gt=datetime.date.today() -
+                                                                       datetime.timedelta(days=7)).order_by("views")[:4]
+        date_ordered_posts = self.model.objects.order_by("date_published")
+        last_post = date_ordered_posts[:1]
         context["four_most_views"] = four_most_views
         return render(request, self.template_name, context)
