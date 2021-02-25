0efdcafb1d84dc1dc1dba886cdb4c0ea733e533e
Amirak <akbariamirreza2001@gmail.com>
2021-02-25 13:33:31 +0330

    template changed - views better structure

--- posts/views.py
+++ posts/views.py
@@ -10,8 +10,11 @@ class IndexPage(TemplateView):
     template_name = "index.html"
     mostViews = Post.objects.order_by("-views")
     lastPosts = Post.objects.order_by("-date_published")
+    mostComments = Post.objects.order_by("-comment_cnt")
 
     def get_context_data(self, **kwargs):
         context = super(IndexPage, self).get_context_data(**kwargs)
         context["top_ten_views"] = self.mostViews[:10]
+        context["four_last_posts"] = self.lastPosts[:4]
+        context["most_comment_carousel"] = self.mostComments[:12]
         return context
